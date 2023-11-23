8c1097867522ce5ec29e08948bf9750d540cb269
onera <charmSmartProgramming@gmail.com>
2023-11-23 18:22:43 +0800

    can connect mongodb but suck in env

--- mongodb.js
+++ mongodb.js
@@ -1,29 +1,30 @@
 // const express = require("express")
 // const mongoose = require("mongoose")
+import mongoose from 'mongoose';
+import express from 'express';
+
+// import { executeCrudOperation } from './crud.js';
+
+const app = express()
+import config from 'dotenv';
+
+// config();
+
+// await executeCrudOperation();
 
 
-import { executeCrudOperation } from './crud.js';
 
-// const app = express()
-import { config } from 'dotenv';
+async function connect() {
+    try {
+        await mongoose.connect("mongodb+srv://charmSmart:Wr6hi9bAAieXkyPR@charmsmart.rpfbnsw.mongodb.net/?retryWrites=true&w=majority")
+        console.log("connected to mongoDB")
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-config();
+connect();
 
-await executeCrudOperation();
-
-
-
-// async function connect() {
-//     try {
-//         await mongoose.connect(uri)
-//         console.log("connected to mongoDB")
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-// connect();
-
-// app.listen(8000, () => {
-//     console.log("Server started on port 8000");
-// })
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+})

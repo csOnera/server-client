bae9d93e020306a871a53f28ff0131b7eadf0966
csOnera <153497796+csOnera@users.noreply.github.com>
2023-12-12 10:54:28 +0800

    Update mongodb.js

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,37 +1,19 @@
-// const express = require("express")
-// const mongoose = require("mongoose")
 import mongoose from 'mongoose';
 import express from 'express';
 import serial from './routes/serial.js';
 import cors from "cors";
-
-// import executeCrudOperation from './crudBackserver.js';
-
-
-
-
-// import { executeCrudOperation } from './crudBackserver.js';
 
 const app = express()
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// await executeCrudOperation();
+
 app.use(cors());
-// app.use(express.json());
+
+// router
 app.use("/serial", serial);
 
-// async function connect() {
-//     try {
-//         await mongoose.connect(process.env.DB_URI)
-//         console.log("connected to mongoDB")
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
-
-// connect();
 
 app.get("/", (req, res) => {
     res.send("home")
